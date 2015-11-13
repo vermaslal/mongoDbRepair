7e97778212784fbedb3b7a8715a8c799f6732a77
sham <vermaslal@gmail.com>
2015-11-13 14:39:04 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+console.log(asdf);
+var MongoClient = require('mongodb').MongoClient;
+var config = require('myconfig').get('mongoDbRepair');
+var mongoConnStr = config.mongoConnectionStr;
+var logger = require('mylogger');
+logger.setLevel(config.logLevel);
+MongoClient.connect(mongoConnStr, function (err, db) {
+  if (err) {
+    logger.error(err);
+  } else {
+    db.command({repairDatabase: 1}, function (err, result) {
+      if (err) {
+        logger.err();
+      } else {
+        logger.log(result);
+      }
+      db.close();
+    });
+  }
+});
